b0497fc1e39c2b8982790f32de6ca894ca61744d
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:03:48 -0600

    Rewriting as Laminas Project package

--- test/Header/PragmaTest.php
+++ test/Header/PragmaTest.php
@@ -1,23 +1,22 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-http for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-http/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-http/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Http\Header;
+namespace LaminasTest\Http\Header;
 
-use Zend\Http\Header\Pragma;
+use Laminas\Http\Header\Pragma;
 
 class PragmaTest extends \PHPUnit_Framework_TestCase
 {
     public function testPragmaFromStringCreatesValidPragmaHeader()
     {
         $pragmaHeader = Pragma::fromString('Pragma: xxx');
-        $this->assertInstanceOf('Zend\Http\Header\HeaderInterface', $pragmaHeader);
-        $this->assertInstanceOf('Zend\Http\Header\Pragma', $pragmaHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\HeaderInterface', $pragmaHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\Pragma', $pragmaHeader);
     }
 
     public function testPragmaGetFieldNameReturnsHeaderName()
