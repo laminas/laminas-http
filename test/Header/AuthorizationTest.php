05717d72cfe611aa62dab08811eab62175378585
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:04:08 -0600

    Rewriting as Laminas Project package

--- test/Header/AuthorizationTest.php
+++ test/Header/AuthorizationTest.php
@@ -1,23 +1,22 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-http for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-http/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-http/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Http\Header;
+namespace LaminasTest\Http\Header;
 
-use Zend\Http\Header\Authorization;
+use Laminas\Http\Header\Authorization;
 
 class AuthorizationTest extends \PHPUnit_Framework_TestCase
 {
     public function testAuthorizationFromStringCreatesValidAuthorizationHeader()
     {
         $authorizationHeader = Authorization::fromString('Authorization: xxx');
-        $this->assertInstanceOf('Zend\Http\Header\HeaderInterface', $authorizationHeader);
-        $this->assertInstanceOf('Zend\Http\Header\Authorization', $authorizationHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\HeaderInterface', $authorizationHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\Authorization', $authorizationHeader);
     }
 
     public function testAuthorizationGetFieldNameReturnsHeaderName()
--- test/Header/ContentMD5Test.php
+++ test/Header/ContentMD5Test.php
@@ -1,23 +1,22 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-http for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-http/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-http/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Http\Header;
+namespace LaminasTest\Http\Header;
 
-use Zend\Http\Header\ContentMD5;
+use Laminas\Http\Header\ContentMD5;
 
 class ContentMD5Test extends \PHPUnit_Framework_TestCase
 {
     public function testContentMD5FromStringCreatesValidContentMD5Header()
     {
         $contentMD5Header = ContentMD5::fromString('Content-MD5: xxx');
-        $this->assertInstanceOf('Zend\Http\Header\HeaderInterface', $contentMD5Header);
-        $this->assertInstanceOf('Zend\Http\Header\ContentMD5', $contentMD5Header);
+        $this->assertInstanceOf('Laminas\Http\Header\HeaderInterface', $contentMD5Header);
+        $this->assertInstanceOf('Laminas\Http\Header\ContentMD5', $contentMD5Header);
     }
 
     public function testContentMD5GetFieldNameReturnsHeaderName()
