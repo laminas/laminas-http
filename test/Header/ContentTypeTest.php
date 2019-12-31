595e97744511f71482610475852766a2dda4924a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:02:07 -0600

    Rewriting as Laminas Project package

--- test/Header/ContentTypeTest.php
+++ test/Header/ContentTypeTest.php
@@ -1,16 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Http
+ * @see       https://github.com/laminas/laminas-http for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-http/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-http/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Http\Header;
+namespace LaminasTest\Http\Header;
 
-use Zend\Http\Header\ContentType;
+use Laminas\Http\Header\ContentType;
 
 class ContentTypeTest extends \PHPUnit_Framework_TestCase
 {
@@ -18,8 +16,8 @@ class ContentTypeTest extends \PHPUnit_Framework_TestCase
     public function testContentTypeFromStringCreatesValidContentTypeHeader()
     {
         $contentTypeHeader = ContentType::fromString('Content-Type: xxx');
-        $this->assertInstanceOf('Zend\Http\Header\HeaderInterface', $contentTypeHeader);
-        $this->assertInstanceOf('Zend\Http\Header\ContentType', $contentTypeHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\HeaderInterface', $contentTypeHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\ContentType', $contentTypeHeader);
     }
 
     public function testContentTypeGetFieldNameReturnsHeaderName()
