e67408ccb202273e87c18d3b3a483a3d4149f227
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:03:44 -0600

    Rewriting as Laminas Project package

--- test/Client/_files/testMultibyteChunkedResponseZF6218.php
+++ test/Client/_files/testMultibyteChunkedResponseZF6218.php
@@ -1,10 +1,9 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-http for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-http/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-http/blob/master/LICENSE.md New BSD License
  */
 
 header("Content-type: text/plain; charset=UTF-8");
