4e8b6f4512ca748cf9ff1e530f7442fffeb359a2
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:03:20 -0600

    Rewriting as Laminas Project package

--- test/Header/EtagTest.php
+++ test/Header/EtagTest.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-http for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-http/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-http/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Http\Header;
+namespace LaminasTest\Http\Header;
 
-use Zend\Http\Header\Etag;
+use Laminas\Http\Header\Etag;
 
 class EtagTest extends \PHPUnit_Framework_TestCase
 {
@@ -17,8 +16,8 @@ class EtagTest extends \PHPUnit_Framework_TestCase
     public function testEtagFromStringCreatesValidEtagHeader()
     {
         $etagHeader = Etag::fromString('Etag: xxx');
-        $this->assertInstanceOf('Zend\Http\Header\HeaderInterface', $etagHeader);
-        $this->assertInstanceOf('Zend\Http\Header\Etag', $etagHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\HeaderInterface', $etagHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\Etag', $etagHeader);
     }
 
     public function testEtagGetFieldNameReturnsHeaderName()
--- test/Header/ViaTest.php
+++ test/Header/ViaTest.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-http for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-http/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-http/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Http\Header;
+namespace LaminasTest\Http\Header;
 
-use Zend\Http\Header\Via;
+use Laminas\Http\Header\Via;
 
 class ViaTest extends \PHPUnit_Framework_TestCase
 {
@@ -17,8 +16,8 @@ class ViaTest extends \PHPUnit_Framework_TestCase
     public function testViaFromStringCreatesValidViaHeader()
     {
         $viaHeader = Via::fromString('Via: xxx');
-        $this->assertInstanceOf('Zend\Http\Header\HeaderInterface', $viaHeader);
-        $this->assertInstanceOf('Zend\Http\Header\Via', $viaHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\HeaderInterface', $viaHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\Via', $viaHeader);
     }
 
     public function testViaGetFieldNameReturnsHeaderName()
