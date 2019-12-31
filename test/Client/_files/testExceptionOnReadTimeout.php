fea66501fc66fb33bbc8d9bb607093fd0d18edac
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:02:53 -0600

    Rewriting as Laminas Project package

--- test/Client/_files/testExceptionOnReadTimeout.php
+++ test/Client/_files/testExceptionOnReadTimeout.php
@@ -1,16 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Http
+ * @see       https://github.com/laminas/laminas-http for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-http/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-http/blob/master/LICENSE.md New BSD License
  */
 
 /**
  * This script does nothing but sleep, and is used to test how
- * Zend_Http_Client handles an exceeded timeout
+ * Laminas_Http_Client handles an exceeded timeout
  */
 
 sleep(5);
