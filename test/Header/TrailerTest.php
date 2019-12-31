4b5c607a7475b43b3c86681b3580c0c2f16b38cd
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:05:11 -0600

    Rewriting as Laminas Project package

--- test/Header/TrailerTest.php
+++ test/Header/TrailerTest.php
@@ -1,23 +1,22 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-http for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-http/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-http/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Http\Header;
+namespace LaminasTest\Http\Header;
 
-use Zend\Http\Header\Trailer;
+use Laminas\Http\Header\Trailer;
 
 class TrailerTest extends \PHPUnit_Framework_TestCase
 {
     public function testTrailerFromStringCreatesValidTrailerHeader()
     {
         $trailerHeader = Trailer::fromString('Trailer: xxx');
-        $this->assertInstanceOf('Zend\Http\Header\HeaderInterface', $trailerHeader);
-        $this->assertInstanceOf('Zend\Http\Header\Trailer', $trailerHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\HeaderInterface', $trailerHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\Trailer', $trailerHeader);
     }
 
     public function testTrailerGetFieldNameReturnsHeaderName()
@@ -52,7 +51,7 @@ class TrailerTest extends \PHPUnit_Framework_TestCase
      */
     public function testPreventsCRLFAttackViaFromString()
     {
-        $this->setExpectedException('Zend\Http\Header\Exception\InvalidArgumentException');
+        $this->setExpectedException('Laminas\Http\Header\Exception\InvalidArgumentException');
         $header = Trailer::fromString("Trailer: xxx\r\n\r\nevilContent");
     }
 
@@ -62,7 +61,7 @@ class TrailerTest extends \PHPUnit_Framework_TestCase
      */
     public function testPreventsCRLFAttackViaConstructor()
     {
-        $this->setExpectedException('Zend\Http\Header\Exception\InvalidArgumentException');
+        $this->setExpectedException('Laminas\Http\Header\Exception\InvalidArgumentException');
         $header = new Trailer("xxx\r\n\r\nevilContent");
     }
 }
--- test/Header/VaryTest.php
+++ test/Header/VaryTest.php
@@ -1,23 +1,22 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-http for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-http/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-http/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Http\Header;
+namespace LaminasTest\Http\Header;
 
-use Zend\Http\Header\Vary;
+use Laminas\Http\Header\Vary;
 
 class VaryTest extends \PHPUnit_Framework_TestCase
 {
     public function testVaryFromStringCreatesValidVaryHeader()
     {
         $varyHeader = Vary::fromString('Vary: xxx');
-        $this->assertInstanceOf('Zend\Http\Header\HeaderInterface', $varyHeader);
-        $this->assertInstanceOf('Zend\Http\Header\Vary', $varyHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\HeaderInterface', $varyHeader);
+        $this->assertInstanceOf('Laminas\Http\Header\Vary', $varyHeader);
     }
 
     public function testVaryGetFieldNameReturnsHeaderName()
@@ -52,7 +51,7 @@ class VaryTest extends \PHPUnit_Framework_TestCase
      */
     public function testPreventsCRLFAttackViaFromString()
     {
-        $this->setExpectedException('Zend\Http\Header\Exception\InvalidArgumentException');
+        $this->setExpectedException('Laminas\Http\Header\Exception\InvalidArgumentException');
         $header = Vary::fromString("Vary: xxx\r\n\r\nevilContent");
     }
 
@@ -62,7 +61,7 @@ class VaryTest extends \PHPUnit_Framework_TestCase
      */
     public function testPreventsCRLFAttackViaConstructor()
     {
-        $this->setExpectedException('Zend\Http\Header\Exception\InvalidArgumentException');
+        $this->setExpectedException('Laminas\Http\Header\Exception\InvalidArgumentException');
         $header = new Vary("xxx\r\n\r\nevilContent");
     }
 }
