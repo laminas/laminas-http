f36279bc777abf992e6c9e23e3bd5582af60e060
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:07:01 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/Client/_files/testCurlGzipData.php
+++ test/Client/_files/testCurlGzipData.php
@@ -1,10 +1,4 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-http for the canonical source repository
- * @copyright https://github.com/laminas/laminas-http/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-http/blob/master/LICENSE.md New BSD License
- */
-
 header('Content-Encoding: gzip');
 echo gzcompress('Success');
