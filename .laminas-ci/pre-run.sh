3fded87e0f88c716ca4147f9b3be064d74bdb654
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-09-09 15:01:26 -0500

    qa: disable fpm enablement on PHP 8.1
    Testing if this is necessary, or in fact causing issues
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -22,7 +22,9 @@ apt update -qq
 # Hack because apache2 package attempts to write to 000-default.conf twice
 apt-get install -o Dpkg::Options::="--force-confnew" -y apache2
 if [[ "${PHP_VERSION}" == "8.1" ]];then
-    switch_sapi -v 8.1 -s fpm:apache
+    # This might not be necessary
+    # switch_sapi -v 8.1 -s fpm:apache
+    echo "Skipping FPM installation on PHP 8.1"
 else
     apt install -y "php${PHP_VERSION}-fpm"
 fi
