4dd93f20356cb4a3746b90f3329bece24d5b6c47
sandesh.as <cod48668@adobe.com>
2025-11-04 14:27:03 +0530

    Allow PHP 8.5 Support
    Signed-off-by: sandesh.as <cod48668@adobe.com>

--- src/Header/MultipleHeaderInterface.php
+++ src/Header/MultipleHeaderInterface.php
@@ -8,6 +8,7 @@ interface MultipleHeaderInterface extends HeaderInterface
      * Convert multiple headers to string representation
      *
      * @param array $headers Array of header instances
+     * @return string
      */
-    public function toStringMultipleHeaders(array $headers): string;
+    public function toStringMultipleHeaders(array $headers);
 }
